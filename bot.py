3fe9900b2ed04a9d4cfe5a8f6a03d6e7e8de8df9
KateZin <90635745+KateZin@users.noreply.github.com>
2023-04-25 22:47:04 +0300

    it works with new token

--- bot.py
+++ bot.py
@@ -1,21 +1,26 @@
 import os
+import sqlite3
 import subprocess
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-
-bot = Bot(token="5951480835:AAGwvuqdswaiMoag81Qw1HMCsiJR9ssKm2E")
+#
+bot = Bot(token="5951480835:AAH3MHJYNlovrilj3wvV6NllZg-pV-gycaI")
 dp = Dispatcher(bot)
 
-logging.basicConfig(filename='.log', encoding='utf-8', level=logging.INFO)
+# logging.basicConfig(filename='.log', encoding='utf-8', level=logging.INFO)
+con = sqlite3.connect('results.db')
+cur = con.cursor()
+cur.execute('DROP TABLE results')
+cur.execute('CREATE TABLE results (surname TEXT, name TEXT, group_num INTEGER, score INTEGER, ass INTEGER)')
 
-
+#
 @dp.message_handler(content_types=types.ContentType.ANY)
 async def process_java(message: types.Message):
     print("hui")
     if message.document:
         print("I an in procces_Java")
-    surname, name, group = "A", "B", "2"
+    surname, name, group = message.caption.split(" ")
     await bot.send_message(chat_id=message.from_user.id, text="Файл получил. Он в очереди на оценку, жди.")
     print(surname, name, group)
     file_id = message.document.file_id
@@ -47,6 +52,15 @@ async def process_java(message: types.Message):
         # если строка не пустая, то записать результат в таблицу
         else:
             res = int(score_file_text)
+            ass = res  # TODO: assessment formula
+            ans = cur.execute(f'SELECT * FROM results WHERE surname = ? AND name = ? AND group_num = ?',
+                              (surname, name, group)).fetchall()
+            if not ans:
+                cur.execute(f'INSERT INTO results VALUES(?, ?, ?, ?, ?)', (surname, name, group, res, ass))
+            else:
+                cur.execute(f'UPDATE results SET score = ?, ass = ? WHERE surname = ? AND name = ? AND group_num = ?',
+                            (res, ass, surname, name, group))
+            con.commit()
             logging.info(f'run {surname} project - {res} points')
             await message.answer(f'Great! Your program compiled and ran successfully. You scored {res} points.')
             return
@@ -55,10 +69,10 @@ async def process_java(message: types.Message):
 async def unknown(message: types.Message):
     await message.answer("Sorry, I didn't understand that command.")
 
-
-# dp.register_message_handler(start, commands=['start'])
-# dp.register_message_handler(process_java, content_types=['document'])
-# dp.register_message_handler(unknown, commands=['help', 'info'])
-
+#
+# # dp.register_message_handler(start, commands=['start'])
+# # dp.register_message_handler(process_java, content_types=['document'])
+# # dp.register_message_handler(unknown, commands=['help', 'info'])
+#
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
